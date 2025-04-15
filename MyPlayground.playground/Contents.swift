1a03b5d30255020cddfd21fec89502f149e2a065
zombiyamo <ars.amnoskmkz@gmail.com>
2025-04-15 09:23:44 +0900

    📝 更新(Contents.swift): コメントを追加し、Optional Bindingの例を追加

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -13,3 +13,18 @@ age = nil
 
 var userId: Int = 1234
 // userId = nil error 'nil' cannot be assigned to type 'Int'
+
+// nil check これOptional Bindingって言うらしい。へー。
+var optionalValue: Int? = 1
+if let value = optionalValue {
+    print("Value: \(value)")
+} else {
+    print("Couldn't bind an optional value")
+}
+
+// 変数名を変えない場合
+if let optionalValue {
+    print("Value: \(optionalValue)")
+} else {
+    print("Couldn't bind an optional value")
+}
