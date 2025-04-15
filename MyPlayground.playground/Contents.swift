8a6ad39723d8e6f3eb6d39e7645e30172eebb348
zombiyamo <ars.amnoskmkz@gmail.com>
2025-04-15 09:15:01 +0900

    📝 ドキュメント(.swift-version): Swiftのバージョンファイルを追加 ♻️ リファクタリング(Contents.swift): コメントと変数の型を追加、値の再代入を修正 ♻️ リファクタリング(contents.xcplayground): 不要な属性を削除

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// 変数var定数let
+var mutable = "Apple"
+mutable = "Lemon"
+
+let immutable = "Apple"
+// immutable = "Lemon" error Cannot assign to value: 'immutable' is a 'let' constant
+
+// Optional
+var age: Int? = 42
+age = nil
+
+var userId: Int = 1234
+// userId = nil error 'nil' cannot be assigned to type 'Int'
