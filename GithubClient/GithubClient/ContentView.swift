6d76e739a27b9fa33a15aad456eb2bba7049f0d3
zombiyamo <ars.amnoskmkz@gmail.com>
2025-04-21 09:46:27 +0900

    ✨ 新機能(ContentView.swift): UIを改善し、テキストスタイルを追加 💡 コメント(ContentView.swift): Dynamic Typeに関する情報を追加

--- GithubClient/GithubClient/ContentView.swift
+++ GithubClient/GithubClient/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            HStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+                    .font(.title)
+                    .foregroundStyle(.blue)
+                    .fontWeight(.bold)
+            }
+            Text("Good evening, world!!")
+                .font(.system(size: 11, weight: .bold)) // 11pt is iOS Mimimum size.
+                .foregroundStyle(.red)
+            Text("Good morning, world!!")
+                .font(.system(size: 17, weight: .bold)) // 11pt is iOS Default size.
+                .foregroundStyle(.green)
+            // Dynamic Type は Largeがデフォルト
+            // https://developer.apple.com/design/human-interface-guidelines/typography#Specifications
         }
-        .padding()
     }
 }
 
