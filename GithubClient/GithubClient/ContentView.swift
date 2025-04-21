0cd0828a1409cb22c955461199c87f5348da6b5b
zombiyamo <ars.amnoskmkz@gmail.com>
2025-04-21 10:02:01 +0900

    ✨ 新機能(GithubClient): GitHubマークのSVG画像を追加 ♻️ リファクタ(ContentView.swift): UIを改善し、GitHubマークを表示

--- GithubClient/GithubClient/ContentView.swift
+++ GithubClient/GithubClient/ContentView.swift
@@ -9,24 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack {
             HStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
-                    .font(.title)
-                    .foregroundStyle(.blue)
-                    .fontWeight(.bold)
+                Image(.githubMark)
+                    .resizable()
+                    .frame(
+                        width: 44.0,
+                        height: 44.0
+                    )
+                VStack(alignment: .leading) {
+                    Text("Owner Name")
+                        .font(.caption)
+                    Text("Repository Name")
+                        .font(.body)
+                        .fontWeight(.semibold)
+                }
             }
-            Text("Good evening, world!!")
-                .font(.system(size: 11, weight: .bold)) // 11pt is iOS Mimimum size.
-                .foregroundStyle(.red)
-            Text("Good morning, world!!")
-                .font(.system(size: 17, weight: .bold)) // 11pt is iOS Default size.
-                .foregroundStyle(.green)
-            // Dynamic Type は Largeがデフォルト
-            // https://developer.apple.com/design/human-interface-guidelines/typography#Specifications
         }
     }
 }
