443e9b8661497591eb3a9af93415551ef22112a3
zombiyamo <ars.amnoskmkz@gmail.com>
2025-04-28 17:47:59 +0900

    ✨ 新機能(RepoListView.swift): ステート管理を追加し、UIを改善 ♻️ リファクタリング(RepoListView.swift): エラーハンドリングとローディング状態を整理

--- GithubClient/GithubClient/Views/RepoListView.swift
+++ GithubClient/GithubClient/Views/RepoListView.swift
@@ -13,7 +13,16 @@ struct RepoListView: View {
   var body: some View {
     NavigationStack {
       Group {
-        if store.error != nil {
+        switch store.state {
+        case .loading:
+          ProgressView("Loading...")
+        case .loaded(let repos):
+          List(repos) { repo in
+            NavigationLink(value: repo) {
+              RepoRow(repo: repo)
+            }
+          }
+        case .failed:
           VStack {
             Text("Failed to load repositories")
             Button(
@@ -29,16 +38,6 @@ struct RepoListView: View {
             )
             .padding()
           }
-        } else {
-          if store.isLoading {
-            ProgressView("Loading...")
-          } else {
-            List(store.repos) { repo in
-              NavigationLink(value: repo) {
-                RepoRow(repo: repo)
-              }
-            }
-          }
         }
       }
       .navigationTitle("Repositories")
@@ -52,16 +51,23 @@ struct RepoListView: View {
   }
 }
 
+enum Stateful<Value> {
+  /// 読込中
+  case loading
+  /// 読み込み失敗、遭遇したエラーを保持
+  case failed(Error)
+  /// 読み込み完了、読み込まれた値を保持
+  case loaded(Value)
+}
+
 @Observable  // インスタンス内部の変更を監視
 @MainActor  // クラス内の処理がメインスレッド上で実行されることが保証される
 class ReposStore {
-  private(set) var repos = [Repo]()
-  private(set) var error: Error? = nil
-  private(set) var isLoading: Bool = false
+  private(set) var state: Stateful<[Repo]> = .loading
   let url = URL(string: "https://api.github.com/orgs/mixigroup/repos")!
 
   func loadRepos() async {
-    isLoading = true
+    state = .loading
     var urlRequest = URLRequest(url: url)
     urlRequest.httpMethod = "GET"
     urlRequest.allHTTPHeaderFields = [
@@ -72,19 +78,19 @@ class ReposStore {
 
     do {
       let (data, response) = try await URLSession.shared.data(for: urlRequest)
-      //      guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-      throw URLError(.badServerResponse)
-      //      }
+      guard let response = response as? HTTPURLResponse,
+        response.statusCode == 200
+      else {
+        throw URLError(.badServerResponse)
+      }
 
       // デコード処理
       let jsonDecoder = JSONDecoder()
       jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
-      repos = try jsonDecoder.decode([Repo].self, from: data)
-      isLoading = false
+      state = .loaded(try jsonDecoder.decode([Repo].self, from: data))
     } catch {
       print("Failed to load repos: \(error)")
-      self.error = error
-      isLoading = false
+      state = .failed(error)
     }
   }
 }
