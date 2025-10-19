e6dcd77ba1f7c0a14030b7b1688f7d6ee493a2ca
zombiyamo <ars.amnoskmkz@gmail.com>
2025-10-19 20:15:16 +0900

    データ表示

--- Today/Today/ReminderListViewController.swift
+++ Today/Today/ReminderListViewController.swift
@@ -8,11 +8,40 @@
 import UIKit
 
 class ReminderListViewController: UICollectionViewController {
+  typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+  typealias SnapShot = NSDiffableDataSourceSnapshot<Int, String>
+
+  var dataSource: DataSource?
 
   override func viewDidLoad() {
     super.viewDidLoad()
     let listLayout = listLayout()
     collectionView.collectionViewLayout = listLayout
+    
+    let cellRegistration = UICollectionView.CellRegistration {
+      (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+      let reminder = Reminder.sampleData[indexPath.item]
+      var contentConfiguration = cell.defaultContentConfiguration()
+      contentConfiguration.text = reminder.title
+      cell.contentConfiguration = contentConfiguration
+    }
+    
+    dataSource = DataSource(collectionView: collectionView) {
+      collectionView,indexPath,itemIdentifier in
+      return collectionView.dequeueConfiguredReusableCell(
+        using: cellRegistration, for: indexPath, item: itemIdentifier)
+    }
+    
+    var snapshot = SnapShot()
+    snapshot.appendSections([0])
+    var reminderTitles = [String]()
+    for reminder in Reminder.sampleData {
+      reminderTitles.append(reminder.title)
+    }
+    snapshot.appendItems(Reminder.sampleData.map{ $0.title })
+    dataSource?.apply(snapshot)
+    
+    collectionView.dataSource = dataSource
   }
 
   private func listLayout() -> UICollectionViewCompositionalLayout {
