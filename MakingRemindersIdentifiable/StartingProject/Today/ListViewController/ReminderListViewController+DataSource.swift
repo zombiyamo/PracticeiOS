77ad40dbb179d04972530dd633a3f598f5557153
zombiyamo <ars.amnoskmkz@gmail.com>
2025-10-21 10:53:36 +0900

    リマインダーをID管理にする

--- MakingRemindersIdentifiable/StartingProject/Today/ListViewController/ReminderListViewController+DataSource.swift
+++ MakingRemindersIdentifiable/StartingProject/Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -5,11 +5,11 @@
 import UIKit
 
 extension ReminderListViewController {
-    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
-    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, Reminder.ID>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Reminder.ID>
 
-    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
-        let reminder = Reminder.sampleData[indexPath.item]
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: Reminder.ID) {
+        let reminder = reminder(withId: id)
         var contentConfiguration = cell.defaultContentConfiguration()
         contentConfiguration.text = reminder.title
         contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
@@ -26,6 +26,16 @@ extension ReminderListViewController {
         var backgroundConfiguration = UIBackgroundConfiguration.listCell()
         backgroundConfiguration.backgroundColor = .todayListCellBackground
         cell.backgroundConfiguration = backgroundConfiguration
+    }
+  
+    func reminder(withId id: Reminder.ID) -> Reminder {
+          let index = reminders.indexOfReminder(withId: id)
+          return reminders[index]
+    }
+  
+    func updateReminder(_ reminder: Reminder) {
+      let index = reminders.indexOfReminder(withId: reminder.id)
+      reminders[index] = reminder
     }
 
     private func doneButtonConfiguration(for reminder: Reminder)
--- MakingRemindersIdentifiable/StartingProject/Today/ListViewController/ReminderListViewController.swift
+++ MakingRemindersIdentifiable/StartingProject/Today/ListViewController/ReminderListViewController.swift
@@ -6,6 +6,7 @@ import UIKit
 
 class ReminderListViewController: UICollectionViewController {
     var dataSource: DataSource?
+    var reminders: [Reminder] = Reminder.sampleData
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,14 +17,14 @@ class ReminderListViewController: UICollectionViewController {
         let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
 
         dataSource = DataSource(collectionView: collectionView) {
-            (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+          (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: Reminder.ID) in
             return collectionView.dequeueConfiguredReusableCell(
                 using: cellRegistration, for: indexPath, item: itemIdentifier)
         }
 
         var snapshot = Snapshot()
         snapshot.appendSections([0])
-        snapshot.appendItems(Reminder.sampleData.map { $0.title })
+        snapshot.appendItems(reminders.map { $0.id })
         dataSource?.apply(snapshot)
 
         collectionView.dataSource = dataSource
